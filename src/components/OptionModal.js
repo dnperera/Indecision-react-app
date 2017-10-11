9bda3c35a77c02df79cd6ae8ed7f0203c3a0b09c
dnperera <dayannp@gmail.com>
2017-10-11 11:45:18 -0700

    Final Version of the app with mobile optimised

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,11 +6,13 @@ const OptionModal =(props) => (
 	<Modal 
 		isOpen={!!props.selectedOption}
 		onRequestClose={props.closeModal}
-		contentLabel="Selected Option">
-
-		<h3>Selected Option </h3>
-			{props.selectedOption && <p>{props.selectedOption}</p>}
-		<button onClick={props.closeModal}>Close</button>
+		contentLabel="Selected Option"
+		closeTimeoutMS={400}
+		className="modal"
+		>
+		<h3 className="modal__title">Selected Option </h3>
+			{props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+		<button className="button" onClick={props.closeModal}>Close</button>
 	</Modal>
 );
 
